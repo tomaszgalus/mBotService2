3cda8f0c9ac24a171acf0c877b284d15a61f617a
Unknown <internetum@gmail.com>
2017-10-06 01:01:58 +0200

    Change intend messages

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Configuration;
 using System.Threading.Tasks;
 
@@ -17,42 +18,87 @@ namespace Microsoft.Bot.Sample.LuisBot
         {
         }
 
-        // Production
         [LuisIntent("None")]
         public async Task NoneIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"Please specify the query :)."); //
+            await context.PostAsync($"Please specify the query :).");
             context.Wait(MessageReceived);
         }
 
         [LuisIntent("Hello")]
         public async Task HelloIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"Hello Dominik....");
+            await context.PostAsync($"Hello Dominik. Nice to hear you again. What do you want to do?");
             context.Wait(MessageReceived);
         }
 
-        [LuisIntent("Payroll")]
-        public async Task PayrollIntent(IDialogContext context, LuisResult result)
+        [LuisIntent("AccountBalance")]
+        public async Task AccountBalanceIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"John 1000 EUR, Maggie 1100 EUR, Steve 1200 EUR");
+            await context.PostAsync($"Which account do you mean?");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("AllAcountsBalance")]
+        public async Task AllAcountsBalanceIntent(IDialogContext context, LuisResult result)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("123456000 - 104 000 EUR");
+            sb.AppendLine("234567000 - 235 000 USD");
+            sb.AppendLine("456789000 - 453 000 EUR");
+            sb.AppendLine("Totatl blance is 559 000 EUR and 235 000 USD.");
+
+            await context.PostAsync(sb.ToString());
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Payments")]
+        public async Task PaymentsIntent(IDialogContext context, LuisResult result)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("150 000 EUR for UPS Invoice 45 / 2017");
+            sb.AppendLine("217 000EUR for Innogy Invoice 12 / 2017");
+            sb.AppendLine("3 400 EUR for T mobile Telephone bill september - 2017");
+            sb.AppendLine("Do you want to accept these payments?");
+
+            await context.PostAsync(sb.ToString());
             context.Wait(MessageReceived);
         }
 
         [LuisIntent("AcceptAll")]
         public async Task AcceptAllIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"All transaction has been accepted");
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("All transaction has been accepted.");
+            sb.AppendLine("I want to remind you that your card expires next month. Do you want to renew it or would you like a new Visa Bussines Gold with 50000 EUR limit.");
+
+            await context.PostAsync(sb.ToString());
             context.Wait(MessageReceived);
         }
 
-        [LuisIntent("RejectAll")]
-        public async Task RejectAllIntent(IDialogContext context, LuisResult result)
+        [LuisIntent("Visa")]
+        public async Task VisaIntent(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync($"All transaction has been rejected");
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Ok.And by the way please note that tomorrow you need to pay taxes.Do you want do it now?");
+
+            await context.PostAsync(sb.ToString());
             context.Wait(MessageReceived);
         }
-         // end production
-           
+
+        [LuisIntent("Goodbye")]
+        public async Task GoodbyeIntent(IDialogContext context, LuisResult result)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Goodbye, Dominik.");
+
+            await context.PostAsync(sb.ToString());
+            context.Wait(MessageReceived);
+        }
     }
 }
